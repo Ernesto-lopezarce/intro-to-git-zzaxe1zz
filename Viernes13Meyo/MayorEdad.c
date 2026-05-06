d8275017ce84953b422a9d6ba0141e062a1c33c4
zzaxe1zz <axelgomezfra09@gmail.com>
2026-05-06 08:19:27 -0600

    primer commit Axel

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,16 @@
+
 #include <stdio.h>
+
+int main(){
+
+    int edad;
+    printf("Indique si edad: ");
+    scanf("%d", &edad);
+    if(edad>=18)
+        printf("Eres Mayor de edad");
+
+    else
+        printf("Te falta vivir bb");
+
+    return 0;
+}
